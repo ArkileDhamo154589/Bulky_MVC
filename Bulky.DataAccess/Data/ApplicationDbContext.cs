8f629ba8d8155b690aed709ffc29869cb9a16b89
dhamo <dhamo@MODULE_LAPTOP_1>
2024-11-18 12:45:10 +0200

    step 2 in ApplicationDbContect create the DbSet also the model builder for our first products

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace Bulky.DataAcess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
-        
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,6 +21,79 @@ namespace Bulky.DataAcess.Data
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
             
+            modelBuilder.Entity<Category>().HasData(
+                new Product { 
+                    Id = 1,
+                    Title = "Fortune of time",
+                    Author = "Billy Spark",
+                    Description = "Description added here",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85, 
+                    Price100 = 80
+                },
+                new Product
+                {
+                    Id = 1,
+                    Title = "Fortune of time",
+                    Author = "Billy Spark",
+                    Description = "Description added here",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                    },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Why you need to learn JS",
+                    Author = "Achilleas Dhamo",
+                    Description = "Learn everything about js from beginner to pro",
+                    ISBN = "SWD9999002",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Why you need to learn .NET",
+                    Author = "Achilleas Dhamo",
+                    Description = "Learn everything about .NET from beginner to pro",
+                    ISBN = "SWD9999002",
+                    ListPrice = 88,
+                    Price = 85,
+                    Price50 = 80,
+                    Price100 = 70
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Why you need to learn React.js",
+                    Author = "Achilleas Dhamo",
+                    Description = "Learn everything about react from beginner to pro",
+                    ISBN = "SWD9999002",
+                    ListPrice = 88,
+                    Price = 85,
+                    Price50 = 80,
+                    Price100 = 70
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Why you need to learn html , css",
+                    Author = "Achilleas Dhamo",
+                    Description = "Learn everything about html css  from beginner to pro",
+                    ISBN = "SWD9999002",
+                    ListPrice = 88,
+                    Price = 85,
+                    Price50 = 80,
+                    Price100 = 70
+                }
+            );
         }
      
     }
